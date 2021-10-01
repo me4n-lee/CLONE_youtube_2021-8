30e7ab108a9e82a8fe07572c9fe7b2599f8954db
keyleeu <minhk.lee21@gmail.com>
2021-10-01 16:27:52 +0900

    #E

---
+++ src/client/js/recorder.js
@@ -0,0 +1,43 @@
+const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
+
+let stream;
+let recorder;
+let videoFile;
+
+
+const handleStop = () => {
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
+  recorder.ondataavailable = (event) => {
+    videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: true,
+  });
+  video.srcObject = stream;
+  video.play();
+};
+
+init();
+
+
+startBtn.addEventListener("click", handleStart);
--- src/models/User.js
+++ src/models/User.js
@@ -4,16 +4,20 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  avatarUrl: String,
+  socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String },
   name: { type: String, required: true },
   location: String,
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+  videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
 });
 
 userSchema.pre("save", async function () {
-    console.log(this.password)
+  if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 5);
-    console.log(this.password)
+  }
 });
 
 const User = mongoose.model("User", userSchema);
