53b4b275cc691a7687f1e27713999b23d22feee6
keyleeu <minhk.lee21@gmail.com>
2021-10-06 16:35:44 +0900

    finish 10/6

--- src/middlewares.js
+++ src/middlewares.js
@@ -10,9 +10,17 @@ const s3 = new aws.S3({
   },
 });
 
-const multerUploader = multerS3({
+const isHeroku = process.env.NODE_ENV === "production";
+
+const s3ImageUploader = multerS3({
   s3: s3,
-  bucket: "youtube-clone-min",
+  bucket: "youtube-clone-min/images",
+  acl: "public-read",
+});
+
+const s3VideoUploader = multerS3({
+  s3: s3,
+  bucket: "youtube-clone-min/videos",
   acl: "public-read",
 });
 
@@ -21,6 +29,7 @@ export const localsMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
     res.locals.siteName = "Wetube";
     res.locals.loggedInUser = req.session.user || {};
+    res.locals.isHeroku = isHeroku;
     next();
 };
 
@@ -49,7 +58,7 @@ export const avatarUpload = multer({
     limits: {
       fileSize: 3000000,
     },
-    storage: multerUploader,
+    storage: isHeroku ? s3ImageUploader : undefined,
 });
 
 export const videoUpload = multer({
@@ -57,5 +66,5 @@ export const videoUpload = multer({
     limits: {
       fileSize: 10000000,
     },
-    storage: multerUploader,
+    storage: isHeroku ? s3VideoUploader : undefined,
 });
