09d9c0ddf535851c5670cf04a021af56b33c5c5c
keyleeu <minhk.lee21@gmail.com>
2021-10-06 20:30:01 +0900

    finish 10/6 #4

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -72,7 +72,7 @@ export const postUpload = async (req, res) => {
     const newVideo = await Video.create({
       title,
       description,
-      fileUrl,
+      fileUrl: video[0].location,
       owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
